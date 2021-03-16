5749ec3ada5ca80f2037e23392aa95ffaaeacd9b
yamunans <32257335+yamunans@users.noreply.github.com>
2021-03-16 17:11:31 -0400

    Add files via upload
    the validation code included inside the epoch for loop

--- lib_ml_train.py
+++ lib_ml_train.py
@@ -9,8 +9,10 @@ from torch.utils.data import DataLoader
 from lib_ml_data   import SSData_path, vol_generator
 from lib_ml_models import VNet_org
 from lib_ml_losses import DiceLoss,dice
+from pytorchtools import EarlyStopping
 import numpy as np
-
+import matplotlib.pyplot as plt
+import time
 
 # data_path contains the dataset currently being used to train the model
 # each dataset contains the training set and the validation set
@@ -24,38 +26,84 @@ import numpy as np
 #num_slices_sag = data_dims[0]
 #num_slices_cor = data_dims[1]
 #num_slices_axl = data_dims[2]
+def visualize_loss(avg_train_losses,avg_valid_losses):
+    # visualize the loss as the network trained
+    fig = plt.figure(figsize=(10,8))
+    plt.plot(range(1,len(avg_train_losses)+1),avg_train_losses, label='Training Loss')
+    plt.plot(range(1,len(avg_valid_losses)+1),avg_valid_losses,label='Validation Loss')
+
+    # find position of lowest validation loss
+    minposs = avg_valid_losses.index(min(avg_valid_losses))+1 
+    
+    plt.axvline(minposs, linestyle='--', color='r',label='Early Stopping Checkpoint')
+
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    
+    plt.xlim(0, len(avg_train_losses)+1) # consistent scale
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    #plt.show()
+    fig.savefig('loss_plot.png', bbox_inches='tight')
+
+
 
 def train_net(data_path,epochs,lr):
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    print('The device being used is =',device)
+
+    print('The number of  epochs is =',epochs)
 
     (training_path,validation_path) = SSData_path(data_path)
     #traingen = vol_generator(training_path)
     #print(len(traingen))
     (xtrain,ytrain) = vol_generator(training_path)
-    #(xval,yval)=vol_generator(validation_path)
+    (xval,yval)=vol_generator(validation_path)
     #n_train= xtrain.shape[0]
     #print(n_train)
     net = VNet_org(in_channels=1, num_class=1)
+    net.to(device)
     # load optimizor
     optimizer = optim.Adam(net.parameters(), lr=lr)
 
     mri_train_loader = DataLoader(xtrain,shuffle=False,batch_size=1)
     mask_train_loader = DataLoader(ytrain,shuffle=False,batch_size=1)
+    mri_val_loader = DataLoader(xval,shuffle=False,batch_size=1)
+    mask_val_loader = DataLoader(yval,shuffle=False,batch_size=1)
     #print(len(train_loader))
     step = 0
     dash =  '-' * 20
     epochend = '='*60
 
+     # initialize the early_stopping object
+    patience =5
+    early_stopping = EarlyStopping(patience=patience, verbose=True)
+    avg_train_losses = []
+    avg_valid_losses = []
+    
+    start = time.time()
     for epoch in range(epochs):
 
         print('EPOCH:',epoch)
-        net.train()
+        
 
-        epoch_loss = 0
+        
         i=1
-
+        
+        ###################
+        # train the model #
+        ###################
+        train_losses=[]
+        net.train() # prep model for training
         for mri_data,mask_data in zip(mri_train_loader,mask_train_loader):
 
             print('ith datavol = ',i)
+            mri_data, mask_data = mri_data.to(device), mask_data.to(device)
             mri_data = mri_data.unsqueeze(0)
             #print('mri_data shape is =',mri_data.shape)
             mask_data = mask_data.unsqueeze(0)
@@ -74,11 +122,63 @@ def train_net(data_path,epochs,lr):
             optimizer.zero_grad()
             LOSS.backward()
             optimizer.step()
-            epoch_loss += LOSS.item()
+            train_losses.append(LOSS.item())
             #print(dash)
         print(epochend)
-        print("epoch %d epochloss:%0.3f" % (epoch, epoch_loss))
-    torch.save(net.state_dict(), 'model_weights.pt')
+        
+
+
+        ######################    
+        # validate the model #
+        ######################
+        net.eval() # prep model for evaluation
+        valid_losses = []
+        with torch.no_grad():
+            count = 0
+            for mri_valdata,mask_valdata in zip(mri_val_loader,mask_val_loader):
+                
+
+                mri_valdata, mask_valdata = mri_valdata.to(device), mask_valdata.to(device)
+                mri_valdata = mri_valdata.unsqueeze(0)
+                #print('mri_data shape is =',mri_data.shape)
+                mask_valdata = mask_valdata.unsqueeze(0)
+                #print('mask_data shape is =',mask_data.shape)
+
+                mri_valdata = torch.tensor(mri_valdata, dtype=torch.float32)
+                mask_valdata = torch.tensor(mask_valdata, dtype=torch.float32)
+                y = net(mri_valdata)
+                valid_losses.append(dice(y, mask_valdata, smooth=1.0))
+
+                count += 1
+                print('validatecount=',count)
+
+        
+        # calculate average loss over an epoch
+        
+        end = time.time()
+        print(f"Runtime of the program is {end - start}")
+        
+        #train_loss = np.average(train_losses)
+        #train_loss = np.mean(train_losses)
+        train_losses = torch.as_tensor(train_losses)
+        train_loss = torch.mean(train_losses)
+        #train_loss = torch.mean(train_losses).detach().cpu().numpy()
+        #valid_loss = np.average(valid_losses)
+        valid_losses = torch.as_tensor(valid_losses)
+        valid_loss = torch.mean(valid_losses)
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
+        print('avg training loss is = ', avg_train_losses)
+        print('avg validation  loss is = 'avg_valid_losses)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+        # early_stopping needs the validation loss to check if it has decresed, 
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(valid_loss, net)
+        visualize_loss(avg_train_losses,avg_valid_losses)
+    #torch.save(net.state_dict(), 'model_weights.pt')
     return net
 
 
