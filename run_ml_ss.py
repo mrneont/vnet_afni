bb323081a4965469a95e457545c7bfd12aeb6546
mrneont <neon.taylor@gmail.com>
2021-03-17 17:26:31 -0400

    run_ml_ss.py: format spacing; show help with no args

--- run_ml_ss.py
+++ run_ml_ss.py
@@ -1,5 +1,6 @@
 import os
-import argparse
+import sys
+import argparse     as argp
 import lib_ml_train as mlt
 
 def dir_path(string):
@@ -9,18 +10,29 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Train the VNet on MRI data and target masks',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-d',"--data_dir", type=dir_path, help="Path to the data directory")
 
-    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
+    intro  = 'Train the VNet on MRI data and target masks'
+    parser = argp.ArgumentParser(description = intro,
+                        formatter_class=argp.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d',"--data_dir", 
+                        type=dir_path, 
+                        help="Path to the data directory")
+
+    parser.add_argument('-e', '--epochs', 
+                        metavar='E', type=int, default=5,
                         help='Number of epochs', dest='epochs')
 
-    parser.add_argument("-l", "--learning_rate", dest="learning_rate", default=0.001, 
-        type=float, help="Learning rate. Default: 0.001")
+    parser.add_argument("-l", "--learning_rate", 
+                        dest="learning_rate", default=0.001, 
+                        type=float, help="Learning rate. Default: 0.001")
     return parser.parse_args()
 
 if __name__ == '__main__':
+
+    # [PT] a trick so that putting in *no* args prompts the help to be
+    # shown
+    if len(sys.argv) == 1 :
+        sys.argv.append('-h')
 
     args = get_args()
     #data_path = 'data/pretrain/pretrain_vnet_res8'
