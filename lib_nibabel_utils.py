0871b06c7fff23ddfd72d81ea88703a579124aef
mrneont <neon.taylor@gmail.com>
2023-02-07 12:32:05 -0500

    lib_nibabel_utils.py: only float32 out if float arr

--- lib_nibabel_utils.py
+++ lib_nibabel_utils.py
@@ -49,7 +49,7 @@ def write_out_nifti_vol( arr3d, fname='dset.nii.gz', outdir=None,
 
     Params
     ------
-    arr3d       : (3D array or pytorch tensor) dset to be written out 
+    arr3d       : (3D numpy array) dset to be written out 
                   as a NIFTI vol
     fname       : (str) output filename, which can include path 
     outdir      : (str) optional way to provide output dir path (could just
@@ -86,6 +86,14 @@ def write_out_nifti_vol( arr3d, fname='dset.nii.gz', outdir=None,
             fname = outdir + fname
         else:
             fname = outdir + '/' + fname
+
+    # all floats to float32 ( **just a start, continue!** )
+    adt = arr3d.dtype
+    DO_FLOAT_HEADER = 0
+    if isinstance(adt, float) :
+        DO_FLOAT_HEADER = 1
+        if not(isinstance(adt, np.float32)) :
+            arr3d = arr3d.astype(np.float32)
 
     if head :
         # temp header, to get a few pieces of info
