b46c6a9ccd3d8a82b27c5630be546f8714559773
yamunans <yamuna.swamy@gmail.com>
2023-07-26 15:18:21 -0400

    populate_dice for entire preicted mask

--- extras/populate_dice.py
+++ extras/populate_dice.py
@@ -12,7 +12,7 @@ from IPython.display import display
 #  Purpose : This python file computes the dice metric of the pred_mask from different epochs and 
 #          target mask and populates the csv file.    
 #
-#  Usage : python populate_dice.py -d 'dir/folder/outdir' -f 'table_file_name'
+#  Usage : python populate_dice.py -d 'dir/folder/outdir' -f 'table_file_name' -m 2
 #
 #
 #****************************************************************************************************
@@ -46,6 +46,11 @@ def get_args():
                             help= "output file", 
                             type=argp.FileType('w'))
 
+    def_m = 1
+    parser.add_argument('-m', "--multiple" ,
+                            help= "multiple", default=def_m, 
+                            type=int)
+
     return parser.parse_args()
 
 
@@ -56,51 +61,54 @@ if __name__ == '__main__':
     args       = get_args()
     foldername = args.data_dir
     filename   = args.filename
+    multiple   = args.multiple
 
-    dn = []
+    
+    df= pd.DataFrame()
+    for idx in range(0, BIG, multiple): # counter for number of epochs 
+    
 
-    for idx in range(BIG): # counter for number of epochs 
-    
-        list_pred_mask = [x for x in glob.glob(os.path.join(foldername, "predmask_OPP_*_train_*.nii.gz"))
-                        if float(x[-20:-18]) == idx]
-        list_target    = [y for y in glob.glob(os.path.join(foldername, "target_*_train*.nii.gz"))
-                        if float(y[-20:-18]) == idx]
-    
-        
+        print('idx =',idx)
+        list_pred_mask = [os.path.basename(x) for x in  glob.glob(os.path.join(foldername,"ch01_ep-*_train_subj-pac_*.nii.gz"))
+                    if float(os.path.basename(x)[8:11]) == idx]
+
+        targ_path_str = os.path.join(foldername, 'target_000_train*.nii.gz')
+        list_target = glob.glob(targ_path_str)
+
         sorted_list_pred_mask = sorted(list_pred_mask) 
         sorted_list_target    = sorted(list_target)
     
         if(len(sorted_list_pred_mask)==0):
             break
+
+        #print('sorted_list_pred_mask =',len(sorted_list_pred_mask))
+        #print('sorted_list_target =',len(sorted_list_target))
     
         #add condition that sorted_list_pred_mask == sorted_list_target
           
-        dice=[]    
-        df1= pd.DataFrame()
+        dice_list=[]    
+        
         for idy in range(len(sorted_list_target)): # counter for dataset
         
             #compute dice for pred_mask and target 
         
-        
+            #print('idy =',idy)
             pred_mask_data1 = nib.load(sorted_list_pred_mask[idy])
             target_data2    = nib.load( sorted_list_target[idy])
             pred_mask_data1 = np.asanyarray(pred_mask_data1.dataobj).astype('float32')
             target_data2    = np.asanyarray(target_data2.dataobj).astype('float32')
-            dice_metric     = get_dice(pred_mask_data1,target_data2)
+            dice            = get_dice(pred_mask_data1,target_data2)
         
         
-            #print("{:0.4f}".format(dice))
-        
-            df1 = df1.append({idx :dice_metric}, ignore_index=True)
+            #print("{:0.4f}".format(dice_metric))
+            dice_list.append(dice)
+            #print('dice_list size',len(dice_list))
+            pd_dice_list = pd.Series(dice_list)
             
-    
-        dn.append(df1)
+        df[f'EPOCH_{idx}'] = pd_dice_list.values
         
-        
-    dn = pd.concat(dn, axis=1)
-    dn.columns = [f'EPOCH_{c}' for c in dn]  
-    display(dn)
+        #display(df)
     
-    dn.to_csv(filename.name + '.csv')
+    df.to_csv(filename.name + '.csv')
 
     print('Loop ended.')
